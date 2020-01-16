20a1008bb1facb4c47caba117686ef17e4bba8da
simon.zhang <simon.zhang@shuyun.com>
2020-01-16 16:47:42 +0800

    simon测试

--- src/main/java/com/jeon/redis/Test.java
+++ src/main/java/com/jeon/redis/Test.java
@@ -1,12 +1,20 @@
 package com.jeon.redis;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.ListDatabasesIterable;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
 import redis.clients.jedis.Jedis;
 
 public class Test {
     public static void main(String[] args) {
 
-        Jedis jedis = new Jedis("47.99.213.83",6380);
-        System.out.println(jedis.ping());
+//        Jedis jedis = new Jedis("47.99.213.83",6380);
+//        System.out.println(jedis.ping());
+        MongoClient mongoClient =new MongoClient("47.99.213.83");
+        ListDatabasesIterable<Document> documents = mongoClient.listDatabases();
+        System.out.println(JSON.serialize(documents));
 
     }
     void test(){
