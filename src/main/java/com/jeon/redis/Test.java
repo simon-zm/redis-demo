37ce5b3aa7d1b2c6f8b9ef7e8fb24982c8391456
simon.zhang <simon.zhang@shuyun.com>
2020-03-09 23:34:22 +0800

    别人提交代码

--- src/main/java/com/jeon/redis/Test.java
+++ src/main/java/com/jeon/redis/Test.java
@@ -8,6 +8,12 @@ public class Test {
         Jedis jedis = new Jedis("47.99.213.83",6380);
         System.out.println(jedis.ping());
         System.out.println("第三方的发达");
+        System.out.println("第三方的发达");
+        System.out.println("第三方的发达");
+        System.out.println("第三方的发达");
+        System.out.println("第三方的发达");
+        System.out.println("第三方的发达");
+
 
     }
     void test(){
