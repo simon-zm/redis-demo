6042ff72e78efba589cc050a14065c1a1a5cf1d6
simon.zhang <simon.zhang@shuyun.com>
2020-03-09 23:38:05 +0800

    的发达

--- src/main/java/com/jeon/redis/Test.java
+++ src/main/java/com/jeon/redis/Test.java
@@ -6,15 +6,6 @@ public class Test {
     public static void main(String[] args) {
 
         Jedis jedis = new Jedis("47.99.213.83",6380);
-        System.out.println(jedis.ping());
-        System.out.println("第三方的发达");
-        System.out.println("第三方的发达");
-        System.out.println("第三方的发达");
-        System.out.println("第三方的发达");
-        System.out.println("第三方的发达");
-        System.out.println("第三方的发达");
-
-
     }
     void test(){
         System.out.println("0000000000000");
