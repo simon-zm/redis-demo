e153c71d4a052a83a0d5a8127d25583f7439ce77
simon.zhang <simon.zhang@shuyun.com>
2020-03-14 17:18:59 +0800

    结束开发

--- src/main/java/com/jeon/redis/Test1.java
+++ src/main/java/com/jeon/redis/Test1.java
@@ -1,5 +1,8 @@
 package com.jeon.redis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: simon.zhang
  * @Date: 2019/12/23
@@ -8,5 +11,9 @@ package com.jeon.redis;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("结束开发");
+
+        //主分支的代码
+        List<String> s =new ArrayList<>();
+        Object o = new Object();
     }
 }
